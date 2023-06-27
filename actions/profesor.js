0ef8e03dc83f0a83c0fb00d1303b3ebcde357367
angel <angelgmorenor@gmail.com>
2023-06-27 01:20:48 -0400

    pruebas

---
+++ actions/profesor.js
@@ -0,0 +1,27 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const retrieveSeccionesByProfId = async (profId) => {
+  const secciones = await prisma.seccion.findMany({
+    where: {
+      habilitado: true,
+      prof_id: profId,
+    },
+  });
+
+  return secciones;
+};
+
+// Usage example
+const profId = 1; // Replace with the desired prof_id
+retrieveSeccionesByProfId(profId)
+  .then((secciones) => {
+    console.log(secciones);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
