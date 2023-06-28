577cfecd647617507242174b968cee7dfe7b5dc7
maquico <1104666@est.intec.edu.do>
2023-06-28 17:55:19 -0400

    estructura registrar

--- app/admin/administrar/layout.js
+++ app/admin/administrar/layout.js
@@ -5,6 +5,7 @@ import BlueDivisionBar from '@/app/components/BlueDivisionBar';
 import NavBarFiltro from '@/app/components/NavBarFiltro';
 import SearchBar from "@/app/components/SearchBar"
 import AddButton from "@/app/components/AddButton"
+import AnadirButton from "@/app/components/AnadirButton";
 
 const Layout = ({}) => {
   const currentRoute = usePathname();
@@ -26,15 +27,24 @@ const Layout = ({}) => {
     }
   } else if (asignaturasActive){
     titulo = "Administrar asignaturas"
+    rutaRegistrar = "/admin/registrar/asignatura"
   } else if (seccionesActive){
     titulo = "Administrar secciones"
+    rutaRegistrar = "/admin/registrar/seccion"
   } else if (aulasActive){
     titulo = "Administrar aulas"
+    rutaRegistrar = "/admin/registrar/aula"
   };
   
   return (
     <>
-      <NavBarFiltro/>
+      
+      {usuariosActive ? <NavBarFiltro /> : null}
+      {usuariosActive ? <div ><Link className="addbutton" href={rutaRegistrar}>  <AddButton/></Link> </div> : 
+        <div className="anadirbuttonstyle">
+        <Link href={rutaRegistrar}>  <AnadirButton /></Link>
+      </div>}
+      
       <BlueDivisionBar/>
       <main className="flex items-left  h-screen">
       <div className="flex flex-col absolute left top-20">
@@ -42,9 +52,7 @@ const Layout = ({}) => {
         <SearchBar/>
       </div>
 
-      <div >
-        <Link className="addbutton" href={rutaRegistrar}>  <AddButton/></Link>
-      </div>
+      
 
     </main>
     </>
