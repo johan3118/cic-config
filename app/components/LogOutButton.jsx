ab3e50e296d9b24222bea9fc9ce9c376ae8935d4
maquico <1104666@est.intec.edu.do>
2023-06-28 01:14:31 -0400

    Logout con icono

--- app/components/LogOutButton.jsx
+++ app/components/LogOutButton.jsx
@@ -1,9 +1,10 @@
 'use client';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 function LogOutButton() {
   return (
     <button className="bg-white hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4  hover:border-transparent rounded-2xl">
-      Button
+      Username <LogoutIcon/>
     </button>
   );
 }
