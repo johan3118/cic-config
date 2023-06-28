07cb69ada895543ceb73675333ca2fd7d6628949
GleidyEspinal <1104225@est.intec.edu.do>
2023-06-28 04:01:48 -0400

    Ajuste backbutton

--- app/components/BackButton.jsx
+++ app/components/BackButton.jsx
@@ -3,7 +3,7 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
 export default function BackButton() {
   return (
-    <button className="bg-white hover:bg-gray-500 hover:text-white text-black-200 font-semibold py-2 px-10 rounded-2xl custom-position"> 
+    <button className="bg-white hover:bg-gray-500 hover:text-white text-black-200 font-semibold py-2 px-3 rounded-2xl custom-position"> 
     <ArrowBackIosNewIcon/>  Regresar
     </button>
   )
