c981a7cc74b3b47607494a254a0a4c0e53dded4d
GleidyEspinal <1104225@est.intec.edu.do>
2023-06-28 06:07:56 -0400

    Ajustes de estructura y pantallas que comparten componentes
    Se agregaron titulos, searchbars y botones a las pantallas de usuarios, asignaturas, secciones y aulas del admin

--- app/admin/asignaturas/page.js
+++ app/admin/asignaturas/page.js
@@ -1,13 +1,19 @@
-import Link from "next/link";
-import SearchBar from "@/app/components/SearchBar.jsx";
+import Link from "next/link"
+import SearchBar from "@/app/components/SearchBar"
+import AnadirButton from "@/app/components/AnadirButton"
 
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen">
+    return (
+      <main className="flex items-center justify-center h-screen">
       <div className="flex flex-col absolute left top-20">
         <h1 className="titulo">Asignaturas</h1>
-        <SearchBar />
+        <SearchBar/>
       </div>
+
+      <div className="anadirbuttonstyle">
+        <Link href="/admin/asignaturas/registrar">  <AnadirButton /></Link>
+      </div>
+
     </main>
-  );
-}
+    )
+  }
