7421fab67d95da36bdc249839a25c241be29fd64
GleidyEspinal <1104225@est.intec.edu.do>
2023-06-28 03:39:08 -0400

    Boton de agregar y de regresar

---
+++ app/components/AddButton.jsx
@@ -0,0 +1,10 @@
+'use client';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+export default function AddButton() {
+  return (
+    <button className='addicon'>
+        <AddCircleOutlineIcon />
+    </button>
+  )
+}
