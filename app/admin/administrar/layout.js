4023804d957e39fe8ad45626a0a5cc40e2f84cee
maquico <1104666@est.intec.edu.do>
2023-06-28 16:40:45 -0400

    nav bar filtro funcional

--- app/admin/administrar/layout.js
+++ app/admin/administrar/layout.js
@@ -1,19 +1,37 @@
 'use client'
 import Link from "next/link"
+import { usePathname } from 'next/navigation';
 import BlueDivisionBar from '@/app/components/BlueDivisionBar';
 import NavBarFiltro from '@/app/components/NavBarFiltro';
 import SearchBar from "@/app/components/SearchBar"
 import AnadirButton from "@/app/components/AnadirButton"
 
 const Layout = ({}) => {
- 
+  const currentRoute = usePathname();
+  const usuariosActive = currentRoute.includes('/admin/administrar/admin') 
+  || currentRoute.includes('/admin/administrar/estudiante') 
+  || currentRoute.includes('/admin/administrar/profesor');
+  const asignaturasActive = currentRoute.includes('/admin/administrar/asignaturas');
+  const seccionesActive = currentRoute.includes('/admin/administrar/secciones');
+  const aulasActive = currentRoute.includes('/admin/administrar/aulas');
+  var titulo = "";
+  if (usuariosActive) {
+    titulo = "Administrar usuarios"
+  } else if (asignaturasActive){
+    titulo = "Administrar asignaturas"
+  } else if (seccionesActive){
+    titulo = "Administrar secciones"
+  } else if (aulasActive){
+    titulo = "Administrar aulas"
+  };
+  
   return (
     <>
-      <NavBarFiltro />
+      <NavBarFiltro/>
       <BlueDivisionBar/>
-      <main className="flex items-center justify-center h-screen">
+      <main className="flex items-left  h-screen">
       <div className="flex flex-col absolute left top-20">
-        <h1 className="titulo"></h1>
+        <h1 className="titulo">{titulo}</h1>
         <SearchBar/>
       </div>
 
--- app/components/NavBarFiltro.jsx
+++ app/components/NavBarFiltro.jsx
@@ -5,41 +5,35 @@ import { usePathname } from 'next/navigation';
 
 function NavBarFiltro() {
   const [navbar, setNavbar] = useState(false);
-  const inactiveStyle = "bg-gray-600";
-  const activeStyle = "active bg-blue-600 ";
+  const inactiveStyle = "bg-gray-200";
+  const activeStyle = "active bg-blue-600 text-white ";
   const currentRoute = usePathname();
   
   return (
-    <div>
-      <nav className="w-full bg-gray-600 flex top-40 left-0 right-0 z-10">
-        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+    <div className="navbarfiltro">
+      <nav className="navbarfiltro w-full bg-gray-200 flex top-40 left-0 right-0 z-10">
+        <div className="justify-between items-center px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-1 md:py-1 md:block">
               <div className="flex items-center">
-                
-                  <ul className="h-screen md:h-auto items-center justify-start md:flex"> {/* Updated alignment to 'justify-start' */}
-                    <li className={currentRoute === '/admin/usuarios/admin' ? activeStyle : inactiveStyle}>
-                      <Link  href="/admin/usuarios/admin" onClick={() => setNavbar(!navbar)}>
-                        <span>Administrador</span>
-                        <div className="absolute left-0 bottom-0 w-0 h-1 transition-all bg-blue-600 group-hover:w-full"></div>
-                      </Link>
-                    </li>
-                    <li className={currentRoute === '/admin/usuarios' ? activeStyle : inactiveStyle}>
-                      <Link  href="/admin/usuarios" onClick={() => setNavbar(!navbar)}>
-                        <span>Profesor</span>
-                        <div className="absolute left-0 bottom-0 w-0 h-1 transition-all bg-blue-600 group-hover:w-full"></div>
-                      </Link>
-                    </li>
-                    <li className={currentRoute === '/admin/asignaturas' ? activeStyle : inactiveStyle}>
-                      <Link  href="/admin/asignaturas" onClick={() => setNavbar(!navbar)}>
-                      <span>Estudiante</span>
-                        <div className="absolute left-0 bottom-0 w-0 h-1 transition-all bg-blue-600 group-hover:w-full"></div>
-                      </Link>
-                    </li>
-                   
-                  </ul>
-                 
-    
+                <div className="h-screen md:h-auto items-center justify-start md:flex">
+                  <Link className={currentRoute === '/admin/administrar/admin' ? activeStyle : inactiveStyle} href="/admin/administrar/admin" onClick={() => setNavbar(!navbar)}>
+                    <span className="mr-4">Administrador</span>
+                    <div className="absolute left-0 bottom-0 w-0 h-1 transition-all bg-blue-600 group-hover:w-full"></div>
+                  </Link>
+                </div>
+                <div className="h-screen md:h-auto items-center justify-start md:flex">
+                  <Link className={currentRoute === "/admin/administrar/profesor" ? activeStyle : inactiveStyle} href="/admin/administrar/profesor" onClick={() => setNavbar(!navbar)}>
+                    <span className="mr-4">Profesor</span>
+                    <div className="absolute left-0 bottom-0 w-0 h-1 transition-all bg-blue-600 group-hover:w-full"></div>
+                  </Link>
+                </div>
+                <div className="h-screen md:h-auto items-center justify-start md:flex">
+                  <Link className={currentRoute === "/admin/administrar/estudiante"  ? activeStyle : inactiveStyle} href="/admin/administrar/estudiante" onClick={() => setNavbar(!navbar)}>
+                    <span className="mr-4">Estudiante</span>
+                    <div className="absolute left-0 bottom-0 w-0 h-1 transition-all bg-blue-600 group-hover:w-full"></div>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
