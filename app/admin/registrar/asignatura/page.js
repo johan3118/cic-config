ab6e0d4a858993c45066f775b73bd5c018202c8d
maquico <1104666@est.intec.edu.do>
2023-06-28 23:21:59 -0400

    page registrar

---
+++ app/admin/registrar/asignatura/page.js
@@ -0,0 +1,13 @@
+'use client'
+import Link from "next/link"
+import AddButton from "@/app/components/AddButton"
+import BackButton from "@/app/components/BackButton"
+
+export default function Home() {
+    return (
+      <main className=" text-3xl">
+        <h1>Registrar asignaturas</h1>
+      </main>
+      
+    )
+  }
