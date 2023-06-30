6ba6d0cfe442a742f7d96b5e18034e4efdb4a387
maquico <1104666@est.intec.edu.do>
2023-06-30 04:40:39 -0400

    ?????????!?!??!??!?!??!

--- app/estudiante/volanteSeleccion/page.js
+++ app/estudiante/volanteSeleccion/page.js
@@ -4,6 +4,9 @@ import Link from "next/link"
 
 export default function Home() {
   const headers = ['CLAVE', 'SEC','NOMBRE', 'CREDITOS'];
+  const id = 1000000;
+  const nombre = "Johan Abiel Contreras";
+  const carrera = "Ingeniería de Software"
 
   const data = [
     { ID: 'IDS325', SEC: '01', NOMBRE: 'Aseguramiento de la Calidad del Software', CREDITOS: '4'}, 
@@ -21,16 +24,18 @@ export default function Home() {
           <div id="ParteArriba" className=" w-full h-20 mb-8 flex flex-col justify-between ">
             <h1 className="text-5xl font-bold mb-6" style={{}}>Volantes</h1>
             <div className=" flex justify-between mb-2 ">
-              <div id="izq" className="w-10 h-10 bg-gray-400"> 
-
+              <div id="izq" className="w-80 h-24 p-3 bg-gray-200 rounded-lg"> 
+                <b>ID: </b> {id} <br/>
+                <b>NOMBRE: </b>{nombre} <br/>
+                <b>CARRERA: </b>{carrera}
               </div>
 
-              <div id="der">
+              <div id="der" className="w-1/2">
 
               </div>
             </div>
           </div>
-        <div className={`mt-14 bg-blue-600 w-full h-2 transform flex justify-center items-center rounded-xl`} ></div>
+        <div className={`mt-24 bg-blue-600 w-full h-2 transform flex justify-center items-center rounded-xl`} ></div>
           <div id="ParteTablas" className=" w-full h-2/3 flex justify-center items-center pt-20">
           <TablaAula headers={headers} data={data}/>
           </div>
