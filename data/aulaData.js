450bae0f15a37c00a7c03a3c8cbaccb2127c46a3
maquico <1104666@est.intec.edu.do>
2023-06-29 23:34:37 -0400

    baila tu cuerpo alegria macarena

--- data/aulaData.js
+++ data/aulaData.js
@@ -1,9 +1,13 @@
-const generateAulaData = (count) => {
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+const generateAulaData = async (count) => {
   const examples = [];
 
   for (let i = 100; i < count; i++) {
     const aula = {
-      aula_clave: `A${i}`,
+      aula_clave: `B${i}`,
       capacidad: Math.floor(Math.random() * 40) + 1,
       fecha_reg: new Date(),
       fecha_mod: null,
@@ -18,8 +22,21 @@ const generateAulaData = (count) => {
   return examples;
 };
 
-// Usage example
-const numberOfExamples = 10; // Change this to generate a different number of examples
-const aulaData = generateAulaData(numberOfExamples);
+const uploadAulaData = async () => {
+  const numberOfExamples = 10; // Change this to generate a different number of examples
+  const aulaData = await generateAulaData(numberOfExamples);
 
-console.log(aulaData);
+  try {
+    for (const aula of aulaData) {
+      await prisma.aula.create({ data: aula });
+    }
+
+    console.log('Aula data uploaded successfully!');
+  } catch (error) {
+    console.error('Error uploading aula data:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+uploadAulaData();
