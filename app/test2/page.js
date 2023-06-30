06f4eb241b67d39e041d0e8d17af41d18147bb89
Huan Hao Wu <huanhaowu28@gmail.com>
2023-06-29 22:24:42 -0400

    ..

--- app/test2/page.js
+++ app/test2/page.js
@@ -6,19 +6,42 @@ import ListCalificacionesDocente from "../components/ListCalificacionesDocente"
 import VolanteSeleccionRegistro from "../components/VolanteSeleccionRegistro"
 import VolanteCalificacionRegistro from "../components/VolanteCalificacionRegistro"
 import PeriodosDropDown from "../components/PeriodosDropDown"
+import TablaAula from "@/app/components/TablaAula"
 
 export default function Home() {
+  
+  const headers = ['CLAVE', 'CAPACIDAD', 'FECHA'];
+
+  const data = [
+    { CLAVE: 'A100', CAPACIDAD: '15', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+    { CLAVE: 'A100', CAPACIDAD: '25', FECHA: '20 / 06 / 2013'},
+  ];
+
   return (
     <main className=" text-3xl">
         <button>
             <Link href="/">TEST 2</Link>
-            <AsignaturasDocente/>
+            <TablaAula headers={headers} data={data}/>
+
+            {/* <AsignaturasDocente/>
             <ListEstudianteDocente/>
             <ListCalificacionesDocente/>
             <VolanteSeleccionRegistro/>
             <VolanteCalificacionRegistro/>
             <br/>
-            <PeriodosDropDown/>
+            <PeriodosDropDown/> */}
         </button>
     </main>
   )
