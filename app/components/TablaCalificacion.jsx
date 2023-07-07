07bc51278c47b9b6f0e398d2cf4c768e62c4e401
angel <angelgmorenor@gmail.com>
2023-07-07 01:42:04 -0400

    editar input de calificaciones y mostrar letra

--- app/components/TablaCalificacion.jsx
+++ app/components/TablaCalificacion.jsx
@@ -1,47 +1,48 @@
-"use client"
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Link from 'next/link';
-import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
-import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
-import CreateIcon from '@mui/icons-material/Create';
-const {disableEntity} = require('@/actions/deshabilitar.js')
-import DeleteIcon from '@mui/icons-material/Delete';
+// TablaCalificacion.js
 
-const TablaCalificacion = ({ headers, data, entity}) => {
-  
-  const handleDelete = (id) => {
-    console.log(id);
-    disableEntity(entity, id);
+import React, { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const TablaCalificacion = ({ headers, data, onInputChange, onFormSubmit }) => {
+  const [activeCell, setActiveCell] = useState(null);
+
+  const handleCellClick = (rowIndex, columnIndex) => {
+    setActiveCell({ rowIndex, columnIndex });
   };
 
-  /* FUNCIONES PARA EL FORMS 
-  function handleInputChange(rowIndex, columnIndex, newValue) {
-  Update the corresponding value in the data array with the new value
-    data[rowIndex][columnIndex] = newValue;
-  }
-  
-  function handleFormSubmit(e, rowIndex, columnIndex) {
+  const handleFormSubmit = (e, rowIndex, columnIndex) => {
     e.preventDefault();
-  Handle the form submission if needed
-  For example, you can perform validation or send the updated data to a server
-  console.log('Form submitted!');
-  } */
-  
+    setActiveCell(null);
+    onFormSubmit(e, rowIndex, columnIndex);
+  };
+
+  const convertToLetterGrade = (calificacion) => {
+    if (calificacion >= 90) return "A";
+    if (calificacion >= 85) return "B+";
+    if (calificacion >= 80) return "B";
+    if (calificacion >= 75) return "C+";
+    if (calificacion >= 70) return "C";
+    if (calificacion >= 60) return "D";
+    return "F";
+  };
 
   return (
-    <TableContainer component={Paper} sx={{ width: 1200, fontFamily: 'Poppins', height: 450, overflowX: 'hidden', zIndex: 10}} className='drop-shadow-xl rounded-3xl'>
+    <TableContainer
+      component={Paper}
+      sx={{ width: 1200, fontFamily: "Poppins", height: 450, overflowX: "hidden", zIndex: 10 }}
+      className="drop-shadow-xl rounded-3xl"
+    >
       <Table sx={{ width: 1200, height: 100 }} aria-label="simple table">
-        <TableHead className='sticky top-0 bg-white' style={{height: 50}}>
+        <TableHead className="sticky top-0 bg-white" style={{ height: 50 }}>
           <TableRow>
             {headers.map((header, index) => (
-              <TableCell align="center" key={index} sx={{fontSize: 10, height: 50, fontWeight: 'bold'}}>
+              <TableCell align="center" key={index} sx={{ fontSize: 10, height: 50, fontWeight: "bold" }}>
                 {header}
               </TableCell>
             ))}
@@ -49,21 +50,49 @@ const TablaCalificacion = ({ headers, data, entity}) => {
         </TableHead>
         <TableBody>
           {data.map((row, rowIndex) => (
-            <TableRow key={rowIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 }, height: 50}} className='bg-gray-100 '>
-              {Object.values(row).map((value, columnIndex) => (
-                <TableCell align="center" key={columnIndex}>
-                  {Number.isInteger(value) && value >= 0 && value <= 100 || value === null ? (
-                  <form onSubmit={(e) => console.log('funciona')
-                  // handleFormSubmit(e, rowIndex, columnIndex)
-                  }>
-                    <input type="text" value={value} onChange={(e) => console.log('funciona')
-                      // handleInputChange(rowIndex, columnIndex, e.target.value)
-                      } className='w-14 rounded-lg py-1 px-2 text-center font-semibold'/>
-                    <button type="submit"></button>
-                  </form> )
-                  : (value)}
-                </TableCell>
-              ))}
+            <TableRow key={rowIndex} sx={{ "&:last-child td, &:last-child th": { border: 0 }, height: 50 }} className="bg-gray-100 ">
+              {Object.entries(row).map(([key, value], columnIndex) => {
+                if (key === "CALIFICACION") {
+                  if (activeCell?.rowIndex === rowIndex && activeCell?.columnIndex === columnIndex) {
+                    return (
+                      <TableCell align="center" key={columnIndex}>
+                        <form onSubmit={(e) => handleFormSubmit(e, rowIndex, columnIndex)}>
+                          <input
+                            type="text"
+                            value={value}
+                            onChange={(e) => onInputChange(rowIndex, columnIndex, e.target.value)}
+                            className="w-14 rounded-lg py-1 px-2 text-center font-semibold"
+                          />
+                          <button type="submit" className="ml-2 text-blue-600 underline">
+                            Guardar
+                          </button>
+                        </form>
+                      </TableCell>
+                    );
+                  } else {
+                    return (
+                      <TableCell align="center" key={columnIndex}>
+                        <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end" }}>
+                          <span className="mr-2">{value}</span>
+                          <span
+                            onClick={() => handleCellClick(rowIndex, columnIndex)}
+                            className="text-blue-600 underline cursor-pointer"
+                            style={{ marginLeft: "12px" }}
+                          >
+                            Editar
+                          </span>
+                        </div>
+                      </TableCell>
+                    );
+                  }
+                } else if (key === "LETRA") {
+                  const calificacion = Number(row["CALIFICACION"]);
+                  const letra = convertToLetterGrade(calificacion);
+                  return <TableCell align="center" key={columnIndex}>{letra}</TableCell>;
+                } else {
+                  return <TableCell align="center" key={columnIndex}>{value}</TableCell>;
+                }
+              })}
             </TableRow>
           ))}
         </TableBody>
--- app/profesor/publicarCalificaciones/page.js
+++ app/profesor/publicarCalificaciones/page.js
@@ -1,54 +1,77 @@
 'use client'
-import Link from "next/link"
-import TablaCalificacion from "@/app/components/TablaCalificacion"
-import SearchBar from "@/app/components/SearchBar"
-import BackButton from "@/app/components/BackButton"
-import { useState } from 'react';
+// Home.js
+
+import Link from "next/link";
+import TablaCalificacion from "@/app/components/TablaCalificacion";
+import SearchBar from "@/app/components/SearchBar";
+import BackButton from "@/app/components/BackButton";
+import { useState } from "react";
 
 export default function Home() {
+  const headers = ["ESTUDIANTE_ID", "NOMBRE", "CORREO", "PROGRAMA", "CALIFICACION", "LETRA"];
 
-  const headers = ['ID', 'NOMBRE', 'CORREO','PROGRAMA', 'CALIFICACION'];
-  
-  const data = [
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100},
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-    { ID: 1000000, NOMBRE: 'Gleidy Joselin Espinal Hernandez', CORREO: 'GleidyEspinal@gmail.com', PROGRAMA: 'IDS', CALIFICACION: 100}, 
-  ];
+  const [data, setData] = useState([
+    { ID: 1000000, NOMBRE: "Gleidy Joselin Espinal Hernandez", CORREO: "GleidyEspinal@gmail.com", PROGRAMA: "IDS", CALIFICACION: 100, LETRA: '-' },
+    // ... other data rows
+  ]);
 
-    return (
-      <div className="mx-20">
-          <div id="ParteArriba" className=" w-full h-auto mt-24 flex flex-col justify-between">
-            <Link href={"/profesor/asignaturas"}> <BackButton/> </Link>
-            <h1 className="text-5xl font-bold mb-6 ">Calificaciones</h1>
-            <div className="flex justify-between mb-6">
-              <div id="izq" className="w-auto h-full bg-gray-100 py-2 px-6 m-2 rounded-lg"> 
-                <h3> <strong>Asignatura:</strong>  Aseguramiento de la Calidad del Software</h3>
-                <div className="flex mt-3"> 
-                  <h3> <strong>Clave:</strong>  IDS350</h3>
-                  <h3 className="ml-14"> <strong>Sección:</strong>  01</h3>
-                </div>
-              </div>
-              <div id="der" className="flex justify-around items-end">
-                <SearchBar/>
-              </div>
+  const [activeCell, setActiveCell] = useState(null);
+
+  const handleInputChange = (rowIndex, columnIndex, newValue) => {
+    const newData = [...data];
+    const rowData = newData[rowIndex];
+    const columnKey = Object.keys(rowData)[columnIndex];
+    rowData[columnKey] = newValue;
+    setData(newData);
+  };
+
+  const handleFormSubmit = (e, rowIndex, columnIndex) => {
+    e.preventDefault();
+    setActiveCell(null); // Reset activeCell when the form is submitted
+    console.log("Form submitted!");
+  };
+
+  const handleCellClick = (rowIndex, columnIndex) => {
+    setActiveCell({ rowIndex, columnIndex }); // Set activeCell on cell click
+  };
+
+  return (
+    <div className="mx-20">
+      <div id="ParteArriba" className="w-full h-auto mt-24 flex flex-col justify-between">
+        <Link href={"/profesor/asignaturas"}>
+          <BackButton />
+        </Link>
+        <h1 className="text-5xl font-bold mb-6 ">Calificaciones</h1>
+        <div className="flex justify-between mb-6">
+          <div id="izq" className="w-auto h-full bg-gray-100 py-2 px-6 m-2 rounded-lg">
+            <h3>
+              <strong>Asignatura:</strong> Aseguramiento de la Calidad del Software
+            </h3>
+            <div className="flex mt-3">
+              <h3>
+                <strong>Clave:</strong> IDS350
+              </h3>
+              <h3 className="ml-14">
+                <strong>Sección:</strong> 01
+              </h3>
             </div>
           </div>
-        <div className={` bg-blue-600 w-full h-2 transform flex justify-center items-center rounded-xl`} ></div>
-        <div id="ParteTablas" className=" w-full h-2/3 flex justify-center items-center py-6">
-            <TablaCalificacion headers={headers} data={data}/>
+          <div id="der" className="flex justify-around items-end">
+            <SearchBar />
+          </div>
         </div>
       </div>
-      
-      // <main className=" text-3xl">
-      //   <button className="w-screen h-screen flex justify-center items-center">
-      //     <Link href="/">Pagina en proceso - Publicar Calificaciones </Link>
-      //   </button>
-      // </main>
-    )
-  }
+      <div className={`bg-blue-600 w-full h-2 transform flex justify-center items-center rounded-xl`}></div>
+      <div id="ParteTablas" className="w-full h-2/3 flex justify-center items-center py-6">
+        <TablaCalificacion
+          headers={headers}
+          data={data}
+          activeCell={activeCell}
+          onInputChange={handleInputChange}
+          onFormSubmit={handleFormSubmit}
+          onCellClick={handleCellClick}
+        />
+      </div>
+    </div>
+  );
+}
